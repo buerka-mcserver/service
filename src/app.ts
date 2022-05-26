bd15af8bddb05a4b578bf4ad9eb1238fde1a37e8
Litfa <admin@litf.com.cn>
2022-05-26 20:47:50 +0800

    feat: 配置文件

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express'
+import config from './utils/config'
 
 const app = express()
 
-app.listen(3000, () => {
+app.get(config.baseUrl, (req, res) => {
+  res.send('service status ok')
+})
+
+app.listen(config.port, () => {
   console.log('http://localhost:3000')
 })
